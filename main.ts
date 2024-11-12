09552d02a317f9f9261bf0438c4fcac74f31b4ea
kingbri <bdashore3@proton.me>
2024-11-11 22:29:48 -0500

    Main: Cleanup
    Signed-off-by: kingbri <bdashore3@proton.me>

--- main.ts
+++ main.ts
@@ -1,12 +1,8 @@
 import { createApi } from "@/api/server.ts";
 import { setupLogger } from "@/common/logging.ts";
 import { loadModel } from "@/common/modelContainer.ts";
-import { basename, parse } from "@std/path";
 
 if (import.meta.main) {
-    const parsedPath = parse(
-        "D:/koboldcpp/Meta-Llama-3-8B-Instruct-Q5_K_M.gguf",
-    );
     await setupLogger();
     await loadModel("D:/koboldcpp/Meta-Llama-3-8B-Instruct-Q5_K_M.gguf", 999);
     createApi();
