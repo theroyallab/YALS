038238fed8392cb7389861ed8ded1514cacf6678
Z <coffeevampirebusiness@gmail.com>
2024-11-23 21:44:54 -0700

    Minimal cpp test

---
+++ bindings/minimal_cpp_test.cpp
@@ -0,0 +1,34 @@
+#include "binding.cpp"
+#include <string>
+#include <thread>
+#include <chrono>
+
+int main() {
+    const std::string modelPath = "/home/blackroot/Desktop/YALS/Models/Meta-Llama-3-8B-Instruct-Q4_K_M.gguf";
+    const auto modelLayers = 999;
+    const auto ctxLen = 8192;
+    const auto prompt = "This is the test prompt";
+    const auto numTokens = 35;
+
+    const auto model = LoadModel(modelPath.c_str(), modelLayers);
+    const auto ctx = InitiateCtx(model, ctxLen, 1);
+    const auto sampler = MakeSampler();
+    GreedySampler(sampler);
+
+    auto readbackBuffer = CreateReadbackBuffer();
+    std::thread inferenceThread(InferToReadbackBuffer,
+      model, sampler, ctx, readbackBuffer, prompt, numTokens,
+      true, true, nullptr, 0, nullptr, 0);
+
+    // Detach the thread if you don't need to wait for it
+    inferenceThread.detach();
+
+    while (!IsReadbackBufferDone(readbackBuffer)) {
+        std::this_thread::sleep_for(std::chrono::milliseconds(5));
+        const char* next = ReadbackNext(readbackBuffer);
+        if (next != nullptr) {
+            std::cout << next;
+            std::flush(std::cout);
+        }
+    }
+}
