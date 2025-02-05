c92e3acb2d4793255b16d6eb96d5fe3aeb08d080
kingbri <8082010+kingbri1@users.noreply.github.com>
2025-02-05 12:21:14 -0500

    API: Fix openAPI handling
    Chat completions uses the "type" property which is often reserved
    by validation libraries. Add an explicit directive to the OpenAPI
    parser to not parse this property as type.
    
    Signed-off-by: kingbri <8082010+kingbri1@users.noreply.github.com>

--- api/OAI/types/chatCompletions.ts
+++ api/OAI/types/chatCompletions.ts
@@ -10,7 +10,7 @@ const ChatCompletionImageUrl = z.object({
 });
 
 const ChatCompletionMessagePart = z.object({
-    type: z.string().nullish().coalesce("text"),
+    type: z.string().nullish().coalesce("text").openapi({ effectType: "input" }),
     text: z.string().nullish(),
     image_url: ChatCompletionImageUrl.nullish(),
 });
