aeff4cf533752de851a9ea3e26ccb54e56a71a9e
kingbri <8082010+kingbri1@users.noreply.github.com>
2025-02-27 00:00:32 -0500

    Bindings: Add option for customizable jobs and CUDA archs
    The CUDA architectures need to be defined as follows:
    - Turing: 75
    - Ampere: 86
    - Ada: 89
    - Blackwell: 120
    
    These are architectures for consumer nvidia GPUS. In addition,
    configure this in actions and make it possible to set the max parallel
    jobs for self builds.
    
    Signed-off-by: kingbri <8082010+kingbri1@users.noreply.github.com>

--- bindings/bindings.ps1
+++ bindings/bindings.ps1
@@ -5,15 +5,28 @@ if (Get-Command cmake -ErrorAction SilentlyContinue) {
     Enter-VsDevShell -VsInstallPath 'C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools' -DevCmdArguments '-arch=x64 -host_arch=x64'
 }
 
+$jobs = if ($env:MAX_JOBS) {
+    $env:MAX_JOBS
+} else {
+    $env:NUMBER_OF_PROCESSORS
+}
+
+$extraCmakeArgs = @()
+
 if ($env:GGML_CUDA -eq 1) {
     Write-Host "CUDA enabled, including in build"
 
-    $extraCmakeArgs += @(
-        "-DGGML_CUDA=ON"
-    )
+    $extraCmakeArgs += "-DGGML_CUDA=ON"
+
+    if ($env:CMAKE_CUDA_ARCHITECTURES) {
+        $extraCmakeArgs += @(
+            "-DCMAKE_CUDA_ARCHITECTURES=$env:CMAKE_CUDA_ARCHITECTURES",
+            "-DGGML_NATIVE=OFF"
+        )
+    }
 }
 
 cmake . -B build -G "Ninja" -DCMAKE_BUILD_TYPE=Release $extraCmakeArgs
-cmake --build build --config Release --target deno_cpp_binding -j $env:NUMBER_OF_PROCESSORS
+cmake --build build --config Release --target deno_cpp_binding -j $jobs
 Copy-Item build/*.dll ../lib
 Copy-Item build/bin/*.dll ../lib
--- bindings/bindings.sh
+++ bindings/bindings.sh
@@ -1,12 +1,31 @@
 #!/bin/bash
 
-if [ "$GGML_CUDA" = "1" ]; then
-    EXTRA_CMAKE_ARGS+="-DGGML_CUDA=ON"
-    echo "CUDA enabled, including in build"
+# Set number of jobs for parallel build
+if [ -n "$MAX_JOBS" ]; then
+    JOBS=$MAX_JOBS
+else
+    JOBS=$(nproc --all)
 fi
 
-cmake . -B build -G "Ninja" -DCMAKE_BUILD_TYPE=Release ${EXTRA_CMAKE_ARGS}
-cmake --build build --config Release --target deno_cpp_binding -j $(nproc --all)
+# Initialize as empty array
+EXTRA_CMAKE_ARGS=()
+
+if [ "$GGML_CUDA" = "1" ]; then
+    EXTRA_CMAKE_ARGS+=("-DGGML_CUDA=ON")
+    echo "CUDA enabled, including in build"
+
+    if [ -n "$CMAKE_CUDA_ARCHITECTURES" ]; then
+        EXTRA_CMAKE_ARGS+=(
+            "-DGGML_NATIVE=OFF" "-DCMAKE_CUDA_ARCHITECTURES=$CMAKE_CUDA_ARCHITECTURES"
+        )
+    fi
+fi
+
+# Join array elements with spaces
+CMAKE_ARGS="${EXTRA_CMAKE_ARGS[*]}"
+
+cmake . -B build -G "Ninja" -DCMAKE_BUILD_TYPE=Release ${CMAKE_ARGS}
+cmake --build build --config Release --target deno_cpp_binding -j ${JOBS}
 
 OS=$(uname -s)
 
