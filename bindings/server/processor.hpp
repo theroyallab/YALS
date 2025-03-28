0808e5fa3752da911794fa301b7512fc5412795a
Z <coffeevampirebusiness@gmail.com>
2025-03-27 23:41:12 -0600

    Minor rearrangement for hard failures in decode.

--- bindings/server/processor.hpp
+++ bindings/server/processor.hpp
@@ -312,22 +312,24 @@ class Processor {
         int32_t decode_result = -1;
         while (true) {
             decode_result = llama_decode(ctx, batch);
+
+            //Decode aborted, this is not a failure, we can redo the decode.
             if (decode_result == 2) {
                 std::this_thread::sleep_for(std::chrono::milliseconds(5));
                 continue;
             }
-            break;
-        }
-        
-        if (decode_result != 0) {
-            //Terminal error, the full batch decode entirely failed. We need to end everything this is not recoverable.
-            for (auto& slot : slots) {
-                if (slot.i_batch >= 0 && slot.i_batch < batch.n_tokens) {
-                    readback_finish(slot.readback_buffer, make_json_status_string(ctx, "BatchDecode", ""));
-                    slot.end(++current_job_index, ctx);
+
+            //TODO:: @Z We can potentially avoid a hard abort depending on the status code. Investigate if possibel.
+            if (decode_result != 0) {
+                for (auto& slot : slots) {
+                    if (slot.i_batch >= 0 && slot.i_batch < batch.n_tokens) {
+                        readback_finish(slot.readback_buffer, make_json_status_string(ctx, "BatchDecode", ""));
+                        slot.end(++current_job_index, ctx);
+                    }
                 }
+                return;
             }
-            return;
+            break;
         }
 
         for (auto& slot : slots) {
