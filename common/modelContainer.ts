ed8e2786856debd2ceff0f2f3fa5e731b12376fb
kingbri <8082010+kingbri1@users.noreply.github.com>
2025-04-19 15:35:40 -0400

    Model: Add lock to prevent multiple loads
    If an operation is in progress, reject other operations.
    
    Signed-off-by: kingbri <8082010+kingbri1@users.noreply.github.com>

--- common/modelContainer.ts
+++ common/modelContainer.ts
@@ -1,13 +1,22 @@
+import { Mutex } from "@core/asyncutil";
+
 import { Model } from "@/bindings/bindings.ts";
 import { ModelConfig } from "@/common/configModels.ts";
 import { logger } from "@/common/logging.ts";
 
 export let model: Model | undefined = undefined;
+const loadLock = new Mutex();
 
 export async function loadModel(
     params: ModelConfig,
     progressCallback?: (progress: number) => boolean,
 ) {
+    if (loadLock.locked) {
+        throw new Error("Another model load operation is in progress. Please wait.");
+    }
+
+    using _lock = await loadLock.acquire()
+
     if (model) {
         if (model?.path.name === params.model_name?.replace(".gguf", "")) {
             throw new Error(
