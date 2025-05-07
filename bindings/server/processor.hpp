1e02699b9541f6e29f0c71d54547ed573bd32254
Z <coffeevampirebusiness@gmail.com>
2025-05-07 11:15:55 -0600

    Corrected parsing argument

--- bindings/server/processor.hpp
+++ bindings/server/processor.hpp
@@ -522,7 +522,7 @@ public:
     int submit_work(
         const std::string& prompt,
         const InferenceArgs& args) {
-        const std::vector<llama_token>& prompt_tokens = tokenizer.tokenize(prompt, args.max_tokens_to_gen);
+        const std::vector<llama_token>& prompt_tokens = tokenizer.tokenize(prompt);
         static int next_id = 1;
         const int request_id = next_id++;
 
